4ba8f7e6c2bb421d96237b475ecd90f746468053
Carnegie Hall <genw.growth@gmail.com>
2024-09-16 18:43:41 -0400

    com

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Navbar from "../components/NavBar.js";
+import Home from "../components/Home.js";
+import About from "../components/About.js";
 
 function App() {
   return (
     <div>
-      <NavBar />
+      <Navbar />
       <Home />
       <About />
     </div>
